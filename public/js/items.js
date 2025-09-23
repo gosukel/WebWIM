3b81fbe51c97ccf252f430196e7b62a0dcd6808f
RJ Routh <94662443+gosukel@users.noreply.github.com>
2025-09-22 23:06:05 -0400

    add expandable row transition and styling

--- public/js/items.js
+++ public/js/items.js
@@ -1,17 +1,40 @@
 document.querySelectorAll(".item-parent").forEach((parent) => {
     parent.addEventListener("click", () => {
-        let childElement = parent.nextElementSibling;
-        if (!childElement.classList.contains("hidden")) {
-            childElement.classList.toggle("hidden");
+        let childElement = parent.nextElementSibling.querySelector(
+            ".item-child-content"
+        );
+        // console.log(childElement);
+        // console.log(parent);
+        if (childElement.classList.contains("show")) {
+            childElement.classList.toggle("show");
+            parent.classList.remove("selected");
+            // console.log(parent);
         } else {
-            document
-                .querySelectorAll(".item-child-locations")
-                .forEach((child) => {
-                    if (!child.classList.contains("hidden")) {
-                        child.classList.add("hidden");
-                    }
-                });
-            childElement.classList.toggle("hidden");
+            // document
+            //     .querySelectorAll(".item-child-content")
+            //     .forEach((child) => {
+            //         if (child.classList.contains("show")) {
+            //             child.classList.remove("show");
+            //             child.parentElement.parentElement.previousElementSibling.classList.remove(
+            //                 "selected"
+            //             );
+            //             // console.log(
+            //             //     child.parentElement.parentElement
+            //             //         .previousElementSibling
+            //             // );
+            //         }
+            //     });
+            document.querySelectorAll(".item-parent.selected").forEach((p) => {
+                p.classList.remove("selected");
+                console.log(p);
+                p.nextElementSibling
+                    .querySelector(".item-child-content")
+                    .classList.remove("show");
+            });
+            childElement.classList.toggle("show");
+            parent.classList.toggle("selected");
+            console.log(parent);
+            console.log(childElement);
         }
     });
 });
