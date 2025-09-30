b65ff772506afc5e687da94a5ecc32817bdc758e
RJ Routh <94662443+gosukel@users.noreply.github.com>
2025-09-29 22:53:46 -0400

    rework sidebar-item to be anchor instead of div

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,15 +5,4 @@ const indexRouter = Router();
 
 indexRouter.get("/", indexController.indexGet);
 
-// items
-// indexRouter.get("/items", indexController.itemsGet);
-// indexRouter.post("/items/new", validateNewItem, indexController.itemsAdd);
-// indexRouter.post("/items/edit", indexController.itemsEdit);
-
-// api calls
-// indexRouter.get("/items/query", indexController.itemsQuery);
-// indexRouter.get("/items/sort", indexController.itemsSort);
-// indexRouter.get("/items/brands", indexController.itemsBrands);
-// indexRouter.get("/items/types", indexController.itemsTypes);
-// indexRouter.get("/process/query", indexController.itemsQuery);
 export default indexRouter;
